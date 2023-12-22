9bea5efde382f45e6bc28eaec72494058435a7b1
sbstm <siswa.relative12@upi.ecu>
2023-12-22 14:41:46 +0700

    Update tsconfig.json and firebase.tsx

--- app/home.jsx
+++ app/home.jsx
@@ -1,7 +1,8 @@
-"use client"
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import {collection,
+'use client'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import {
+  collection,
   addDoc,
   getDoc,
   updateDoc,
@@ -9,78 +10,113 @@ import {collection,
   setDoc,
   onSnapshot,
   deleteDoc,
-  doc} from 'firebase/firestore';
-  import { db } from './firebase';
-import Link from 'next/link';
-  
-  const RatingStar = ({ rating }) => {
-    const stars = [];
-    for (let i = 0; i < 5; i++) {
-       if (i < rating) {
-         stars.push(<svg className="w-4 h-4 fill-current text-yellow-400" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21L16.54 13.94L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.94L5.82 21L12 17.27Z"/></svg>);
-       } else {
-         stars.push(<svg className="w-4 h-4 fill-current text-gray-400" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21L16.54 13.94L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.94L5.82 21L12 17.27Z"/></svg>);
-       }
+  doc,
+} from 'firebase/firestore'
+import { db, storage } from './firebase'
+import Link from 'next/link'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage'
+
+const RatingStar = ({ rating }) => {
+  const stars = []
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      stars.push(
+        <svg
+          className="w-4 h-4 fill-current text-yellow-400"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 17.27L18.18 21L16.54 13.94L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.94L5.82 21L12 17.27Z" />
+        </svg>
+      )
+    } else {
+      stars.push(
+        <svg className="w-4 h-4 fill-current text-gray-400" viewBox="0 0 24 24">
+          <path d="M12 17.27L18.18 21L16.54 13.94L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.94L5.82 21L12 17.27Z" />
+        </svg>
+      )
     }
-    return <div className="flex space-x-1">{stars}</div>;
-   };
-
-
-     
+  }
+  return <div className="flex space-x-1">{stars}</div>
+}
 
 export default function Home() {
-  const [items, setItems] = useState([{ saran: '', foto: '', rating: '', acc: false }]);
-  const [newItems, setNewItems] = useState({ saran: '', foto: '', rating: '', acc: false });
-  const [total, setTotal] = useState(0);
+  const [items, setItems] = useState([{ saran: '', rating: 0, acc: false }])
+  const [newItems, setNewItems] = useState({
+    saran: '',
+    rating: 0,
+    acc: false,
+  })
+  const [foto, setFoto] = useState(null)
+  const [total, setTotal] = useState(0)
 
-
-  const addItem = async (e) =>  {
-    e.preventDefault();
-    if (newItems.saran !== '') {
+  const addItem = async (e) => {
+    e.preventDefault()
+    if (newItems.saran !== '' && newItems.rating !== '' && foto !== null) {
+      const storageRef = ref(storage, `images/${foto.name}`)
+      const uploadTask = uploadBytesResumable(storageRef, foto)
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log(`Upload is ${progress}% done`)
+        },
+        (error) => {
+          console.log(error)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL)
+          })
+        }
+      )
       await addDoc(collection(db, 'items'), {
         saran: newItems.saran.trim(),
-        foto: newItems.foto,
         rating: newItems.rating,
-        acc: newItems.acc = false
-      });
-      setNewItems({ saran: '', foto: '', rating: '', acc: false });
+        acc: (newItems.acc = false),
+      })
+      setNewItems({ saran: '', rating: 0, acc: false })
     }
-  };
-
+  }
 
   useEffect(() => {
-    const q = query(collection(db, 'items'));
+    const q = query(collection(db, 'items'))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let temp = [];
+      let temp = []
 
       querySnapshot.forEach((doc) => {
-        temp.push({ ...doc.data(), id: doc.id });
-      });
-      setItems(temp);
+        temp.push({ ...doc.data(), id: doc.id })
+      })
+      setItems(temp)
 
       // Read total from temp
       const calculateTotal = () => {
         const totalPrice = temp.reduce(
           (sum, item) => sum + parseFloat(item.price),
           0
-        );
-        setTotal(totalPrice);
-      };
-      calculateTotal();
-    });
+        )
+        setTotal(totalPrice)
+      }
+      calculateTotal()
+    })
 
-    return () => unsubscribe();
-  }, []);
+    return () => unsubscribe()
+  }, [])
 
   const updateItem = (id, acc) => {
-    setItems(items.map(item => item.id === id ? { ...item, acc } : item));
- };
- const filterAcceptedItems = items.filter(item => item.acc);
- const filterAcceptedItems2 = items.filter(item => !item.acc);
+    setItems(items.map((item) => (item.id === id ? { ...item, acc } : item)))
+  }
+  const filterAcceptedItems = items.filter((item) => item.acc)
+  const filterAcceptedItems2 = items.filter((item) => !item.acc)
 
- const deleteItem = async (id) => {
-  await deleteDoc(doc(db, 'items', id));
-};
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, 'items', id))
+  }
 
   return (
     <>
@@ -89,105 +125,140 @@ export default function Home() {
           <h4>saran</h4>
           <input
             value={newItems.saran}
-            onChange={(e) => setNewItems({ ...newItems, saran: e.target.value })}
+            onChange={(e) =>
+              setNewItems({ ...newItems, saran: e.target.value })
+            }
             type="text"
             className=""
             id="saran"
             placeholder="silahkan di isi"
           />
           <h4>foto</h4>
-          <input value={newItems.foto}
-          onChange={(e) => setNewItems({ ...newItems, foto: e.target.value })}
-          type="file" className="" id="foto" />
+          <input
+            value={newItems.foto}
+            onChange={(e) => setNewItems({ ...newItems, foto: e.target.value })}
+            type="file"
+            max={1}
+            maxsize={10000}
+            className=""
+            id="foto"
+          />
           <h4>rating</h4>
-          <input value={newItems.rating}
-           
-          onChange={(e) => setNewItems({ ...newItems, rating: e.target.value })}
-          type="range" className="" id="rating" max={5} />
+          <input
+            value={newItems.rating}
+            onChange={(e) =>
+              setNewItems({ ...newItems, rating: e.target.value })
+            }
+            type="range"
+            className=""
+            id="rating"
+            max={5}
+          />
           <button onClick={addItem}>submit</button>
         </form>
-      <h1 className="text-2xl font-bold mb-4">App Peningkatan Proses Produksi</h1>
-      <div className="w-full">
-        <table className="w-full">
-          <thead>
-            <tr className="bg-gray-200 text-slate-900">
-              <th className="p-3 font-bold">Saran</th>
-              <th className="p-3 font-bold">Foto</th>
-              <th className="p-3 font-bold">Rating</th>
-              <th className="p-3 font-bold">Acc</th>
-              <th className="p-3 font-bold">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filterAcceptedItems.map(item => (
-              <tr key={item.id} className="text-center border-b border-gray-300">
-              <td className="p-3">{item.saran}</td>
-              <td className="p-3">{item.foto}</td>
-              <td className="p-3">
-               <RatingStar rating={item.rating} />
-             </td>
-              <td className="p-3">
-                <button
-                  onClick={() => updateItem(item.id, !item.acc)}
-                  className="p-2 border border-slate-900 hover:bg-slate-900"
+        <h1 className="text-2xl font-bold mb-4">
+          App Peningkatan Proses Produksi
+        </h1>
+        <div className="w-full">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-gray-200 text-slate-900">
+                <th className="p-3 font-bold">Saran</th>
+                <th className="p-3 font-bold">Foto</th>
+                <th className="p-3 font-bold">Rating</th>
+                <th className="p-3 font-bold">Acc</th>
+                <th className="p-3 font-bold">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterAcceptedItems.map((item) => (
+                <tr
+                  key={item.id}
+                  className="text-center border-b border-gray-300"
                 >
-                  {item.acc ? '✅' : '❌'}
-                </button>
-              </td>
-              <td className="p-3">
-                <button
-                  onClick={() => deleteItem(item.id)}
-                  className="p-2 border border-slate-900 hover:bg-slate-900"
-                >
-                  X
-                </button> <p>{item.acc}</p>
-              </td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-        <table className="w-full">
-          <thead>
-            <tr className="bg-gray-200 text-slate-900">
-              <th className="p-3 font-bold">Saran</th>
-              <th className="p-3 font-bold">Foto</th>
-              <th className="p-3 font-bold">Rating</th>
-              <th className="p-3 font-bold">Acc</th>
-              <th className="p-3 font-bold">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filterAcceptedItems2.map(item => (
-              <tr key={item.id} className="text-center border-b border-gray-300">
-              <td className="p-3">{item.saran}</td>
-              <td className="p-3">{item.foto}</td>
-              <td className="p-3">
-               <RatingStar rating={item.rating} />
-             </td>
-              <td className="p-3">
-                <button
-                  onClick={() => updateItem(item.id, !item.acc)}
-                  className="p-2 border border-slate-900 hover:bg-slate-900"
-                >
-                  {item.acc ? '✅' : '❌'}
-                </button>
-              </td>
-              <td className="p-3">
-                <button
-                  onClick={() => deleteItem(item.id)}
-                  className="p-2 border border-slate-900 hover:bg-slate-900"
-                >
-                  X
-                </button> <p>{item.acc}</p>
-              </td>
-            </tr>
-            ))}
+                  <td className="p-3">{item.saran}</td>
+                  <td className="p-3">{item.foto}</td>
+                  <td className="p-3">
+                    <RatingStar rating={item.rating} />
+                  </td>
+                  <td className="p-3">
+                    <button
+                      onClick={() => updateItem(item.id, !item.acc)}
+                      className="p-2 border border-slate-900 hover:bg-slate-900"
+                    >
+                      {item.acc ? '✅' : '❌'}
+                    </button>
+                  </td>
+                  <td className="p-3">
+                    <button
+                      onClick={() => deleteItem(item.id)}
+                      className="p-2 border border-slate-900 hover:bg-slate-900"
+                    >
+                      X
+                    </button>{' '}
+                    <p>{item.acc}</p>
+                  </td>
+                </tr>
+              ))}
             </tbody>
-        </table>
-        <Link href="/login"><p className="p-2 border border-slate-900 hover:bg-slate-900">Login</p></Link>
-        <Link href="/register"><p className="p-2 border border-slate-900 hover:bg-slate-900">register</p></Link>
-      </div>
+          </table>
+          <table className="w-full">
+            <thead>
+              <tr className="bg-gray-200 text-slate-900">
+                <th className="p-3 font-bold">Saran</th>
+                <th className="p-3 font-bold">Foto</th>
+                <th className="p-3 font-bold">Rating</th>
+                <th className="p-3 font-bold">Acc</th>
+                <th className="p-3 font-bold">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterAcceptedItems2.map((item) => (
+                <tr
+                  key={item.id}
+                  className="text-center border-b border-gray-300"
+                >
+                  <td className="p-3">{item.saran}</td>
+                  <td className="p-3">
+                    {' '}
+                    <image />
+                  </td>
+                  <td className="p-3">
+                    <RatingStar rating={item.rating} />
+                  </td>
+                  <td className="p-3">
+                    <button
+                      onClick={() => updateItem(item.id, !item.acc)}
+                      className="p-2 border border-slate-900 hover:bg-slate-900"
+                    >
+                      {item.acc ? '✅' : '❌'}
+                    </button>
+                  </td>
+                  <td className="p-3">
+                    <button
+                      onClick={() => deleteItem(item.id)}
+                      className="p-2 border border-slate-900 hover:bg-slate-900"
+                    >
+                      X
+                    </button>{' '}
+                    <p>{item.acc}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Link href="/login">
+            <p className="p-2 border border-slate-900 hover:bg-slate-900">
+              Login
+            </p>
+          </Link>
+          <Link href="/register">
+            <p className="p-2 border border-slate-900 hover:bg-slate-900">
+              register
+            </p>
+          </Link>
+        </div>
       </div>
     </>
-  );
-            }
+  )
+}
--- confix/firebase.tsx
+++ confix/firebase.tsx
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAtgs6L7VdQLd_GssVI8oxvVDaqAoewWg0",
@@ -15,6 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
 const analytics = getAnalytics(app);
 export const db = getFirestore(app)
